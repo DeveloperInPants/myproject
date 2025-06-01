7bd9438b213cc36741f1d69e7e55472dca489dda
DeveloperInPants <147731303+DeveloperInPants@users.noreply.github.com>
2025-06-02 00:39:33 +0300

    фикс старт

--- main.js
+++ main.js
@@ -30,17 +30,3 @@ document.querySelector('.close-btn').addEventListener('click', () => {
     modal.style.display = 'none';
     if (currentGame) currentGame.stop();
 });
-function startGame(GameClass) {
-    console.log("Запуск игры"); // Должно появиться в консоли
-    console.log(GameClass); // Должен вывести класс Pong
-    
-    gameTitle.textContent = GameClass.title;
-    gameControls.textContent = GameClass.controls;
-    canvas.width = 800;
-    canvas.height = 500;
-    
-    currentGame = new GameClass(canvas);
-    modal.style.display = 'block';
-    
-    console.log("Игра создана"); // Проверка
-}
